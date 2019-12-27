c75ed84e9e9de75bc78e68790bca81a9fab46093
James Tomasino <james@tomasino.org>
2019-12-27 17:05:16 +0000

    soma uses cache to speed up

--- .functions/soma.sh
+++ .functions/soma.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
 soma() {
   if [ -z "$1" ]; then
-    station="$(curl -s https://somafm.com/listen/ | \
-      awk -F '[<>]' '/MP3:/ { print $4 }' | \
-      awk -F '"' '{print $2}' | \
-      tr -d \\/ | \
-      sed 's|.pls$||' | \
-      fzf \
+
+    tempfile="${XDG_CACHE_HOME}/soma-stations.txt" || return
+    if test "$(find "$tempfile" -mmin +$((60*24)) 2> /dev/null)"; then
+      rm "$tempfile"
+    fi
+
+    if [ ! -f "$tempfile" ]; then
+      curl -s https://somafm.com/listen/ | awk -F '[<>]' '/MP3:/ { print $4 }' | awk -F '"' '{print $2}' | tr -d \\/ | sed 's|.pls$||' > "$tempfile"
+    fi
+
+    station="$(fzf \
       --multi \
       --select-1 \
-      --exit-0)"
-    mplayer -playlist "http://somafm.com/${station}.pls"
+      --exit-0 < "$tempfile")"
+    if [ -n "$station" ]; then
+      mplayer -playlist "http://somafm.com/${station}.pls"
+    fi
   else
     mplayer -playlist "http://somafm.com/${1}.pls"
   fi
