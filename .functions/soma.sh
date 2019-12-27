c56747b9f030afe4754aeda119581aee9679d2a9
James Tomasino <james@tomasino.org>
2019-12-27 16:42:15 +0000

    soma uses fzf

--- .functions/soma.sh
+++ .functions/soma.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 soma() {
-  if [ "$1" = "list" ]; then
-    curl -s https://somafm.com/listen/ | awk -F '[<>]' '/MP3:/ { print $4 }' | awk -F '"' '{print $2}' | tr -d \\/ | sed 's|.pls$||'
+  if [ -z "$1" ]; then
+    station="$(curl -s https://somafm.com/listen/ | \
+      awk -F '[<>]' '/MP3:/ { print $4 }' | \
+      awk -F '"' '{print $2}' | \
+      tr -d \\/ | \
+      sed 's|.pls$||' | \
+      fzf \
+      --multi \
+      --select-1 \
+      --exit-0)"
+    mplayer -playlist "http://somafm.com/${station}.pls"
   else
     mplayer -playlist "http://somafm.com/${1}.pls"
   fi
