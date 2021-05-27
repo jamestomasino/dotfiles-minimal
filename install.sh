8204a5c45bc3e80df83c31c13106a7a61bc2b64b
James Tomasino <james@tomasino.org>
2021-05-27 10:19:41 +0000

    greatly simplified install shell script

--- install.sh
+++ install.sh
@@ -6,23 +6,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 tryfiles () {
   local path="$1"
-  if [ -d "${HOME}/${path}" ]; then
-    cd "${DIR}/${path}"
+  if [ -d "${DIR}/${path}" ]; then
+    pushd "${DIR}/${path}" || return
     mkdir -p "${HOME}/${path}"
     for f in *; do
       local newpath="${path}/${f}"
-      if [ -d "${DIR}/${newpath}" ]; then
-        tryfiles "${newpath}"
-      else
-        if [ -e "${HOME}/${newpath}" ]; then
-          printf "File Exists, Skipping: %s\\n" "${HOME}/${newpath}"
-        else
-          ln -sfn "${DIR}/${newpath}" "${HOME}/${newpath}"
-          printf "Linking: %s/%s\\n" "${DIR}" "${newpath}"
-        fi
-      fi
+      tryfiles "${newpath}"
     done
-    cd ..
+    popd || return
   else
     if [ -e "${HOME}/${path}" ]; then
       printf "File Exists, Skipping: %s\\n" "${HOME}/${path}"
@@ -33,39 +24,17 @@ tryfiles () {
   fi
 }
 
-function silentSsh {
-    local connectionString="$1"
-    local commands="$2"
-    if [ -z "$commands" ]; then
-        commands=`cat`
-    fi
-    ssh -T $connectionString "$commands"
-}
-
-if [ "$1" = "push" ]; then
-  if [ -n "$2" ]; then
-    rsync -rvhe ssh --progress "${DIR}/" --exclude 'README.md' --exclude 'install.sh' --exclude 'uninstall.sh' --exclude '.git' "$2":~
-    silentSsh $2 <<'EOC'
-    curl -sfLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    curl -sfLo "$HOME/.config/vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-    mkdir -p "$HOME/.cache"
-    mkdir -p "$HOME"/.local/share/vim/{undo,swap,backup}
-EOC
-  else
-    printf "push requires a destination: './install push someserver.com'\\n"
+# main loop, ignoring some key files
+for x in *; do
+  if [ "$x" != ".git" ] && [ "$x" != "." ] && [ "$x" != ".." ] && [ "$x" != "install.sh" ]&& [ "$x" != "uninstall.sh" ] && [ "$x" != "README.md" ]; then
+    tryfiles "$x"
   fi
-else
-  for x in *; do
-    if [ "$x" != ".git" ] && [ "$x" != "." ] && [ "$x" != ".." ] && [ "$x" != "install.sh" ]&& [ "$x" != "uninstall.sh" ] && [ "$x" != "README.md" ]; then
-      tryfiles "$x"
-    fi
-  done
+done
 
-  if [ ! -e "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
-    curl -sfLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  fi
+if [ ! -e "$HOME/.local/share/nvim/site/autoload/plug.vim" ]; then
+  curl -sfLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
 
-  if [ ! -e "$HOME/.config/vim/autoload/plug.vim" ]; then
-    curl -sfLo "$HOME/.config/vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-  fi
+if [ ! -e "$HOME/.config/vim/autoload/plug.vim" ]; then
+  curl -sfLo "$HOME/.config/vim/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
