713c00c4204b7b3d108c963381c67af971062d8b
James Tomasino <james@tomasino.org>
2022-03-16 00:27:47 +0000

    even more simplified date calc

--- .functions/julian.sh
+++ .functions/julian.sh
@@ -2,7 +2,6 @@
 
 julian () {
   d=$(date +%s)
-  todayJd=$( echo "x = $d; x / 86400 + 2440587" | bc)
-  timeJd="$(echo "x = ($d - 43200 ) % 86400; scale=5; x / 86400" | bc)"
-  printf "%s%s\n" "$todayJd" "$timeJd"
+  todayJd=$( echo "x = $d; scale=5; x / 86400 + 2440587.5" | bc)
+  printf "%s\n" "$todayJd"
 }
