f17f182f323ee3b750cffe96984a93fa5c4efe7e
James Tomasino <james@tomasino.org>
2022-03-22 15:02:23 +0000

    better maildirect script

--- bin/maildirect.sh
+++ bin/maildirect.sh
@@ -1,11 +1,27 @@
 #!/bin/sh
 
-html=${1:-index.html}
-to=${2:-${MAILGUN_TEST_TO}}
+if [ -z "${MAILGUN_API}" ]; then
+    echo "Must provide MAILGUN_API in environment" 1>&2
+    exit 1
+fi
+
+if [ -z "${MAILGUN_API_ENDPOINT}" ]; then
+    echo "Must provide MAILGUN_API_ENDPOINT in environment" 1>&2
+    exit 1
+fi
+
+to=${1:-${MAILGUN_TEST_TO}}
+
+if [ -z "${to}" ]; then
+    echo "Must provide email destination" 1>&2
+    exit 1
+fi
+
+html=${2:-index.html}
 subject=${3:-Test Email}
 
 curl -s --user "api:${MAILGUN_API}" \
-https://api.eu.mailgun.net/v3/mg.tomasino.is/messages \
+"${MAILGUN_API_ENDPOINT}" \
 -F from="${MAILGUN_TEST_FROM}" \
 -F to="${to}" \
 -F subject="${subject}" \
