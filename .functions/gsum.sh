8c348f2c4f2df6768d7f04bf332ec06e315b483b
James Tomasino <james@tomasino.org>
2023-04-16 16:18:48 +0000

    Added a brief message on line one of git commit message prompt in gsum.sh function.
    The gsum.sh function in .functions directory has been updated to include a brief message on line one of the git commit message prompt. This will help users provide a more descriptive commit message for their changes. The updated function now includes the query variable with the new message and passes it to sgpt for user input. The commit message is then printed and the user is prompted to confirm their changes with the new message.

--- .functions/gsum.sh
+++ .functions/gsum.sh
@@ -6,7 +6,7 @@
 gsum() {
   diff="$(git --no-pager diff --cached)"
   if [ -n "$diff" ]; then
-    query="Generate a descriptive git commit message based on the following code changes: \n\n${diff}"
+    query="Generate a descriptive git commit message based on the following code changes with a brief message on line one and the rest below: \n\n${diff}"
     commit_message="$(sgpt "${query}")"
     printf "%s\n" "$commit_message"
     read -rp "Do you want to commit your changes with this commit message? [y/N] " response
