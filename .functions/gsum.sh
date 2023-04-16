c4419d3557cb35514c6c9bc218770f0baa3d2a87
James Tomasino <james@tomasino.org>
2023-04-16 16:14:37 +0000

    Refactor gsum function to only run on staged content and abort if there is no diff content. If changes exist, generate a descriptive git commit message based on the code changes and prompt the user to commit with the generated message.

--- .functions/gsum.sh
+++ .functions/gsum.sh
@@ -1,15 +1,19 @@
-## git summarize ##
-# Leverage SGPT to produce intelligent and context-sensitive git commit messages.
-# By providing one argument, you can define the type of semantic commit (e.g. feat, fix, chore).
-# When supplying two arguments, the second parameter allows you to include more details for a more explicit prompt.
+#!/bin/sh
+
+# Leverage SGPT to produce intelligent and context-sensitive git commit messages
+# - Runs only on staged content
+# - Aborts if there is no diff content
 gsum() {
-  query="Generate git commit message using semantic versioning. My changes: $(git --no-pager diff --cached)"
-  commit_message="$(sgpt txt "$query")"
-  printf "%s\n" "$commit_message"
-  read -rp "Do you want to commit your changes with this commit message? [y/N] " response
-  if [[ $response =~ ^[Yy]$ ]]; then
-    git add . && git commit -m "$commit_message"
-  else
-    echo "Commit cancelled."
+  diff="$(git --no-pager diff --cached)"
+  if [ -n "$diff" ]; then
+    query="Generate a descriptive git commit message based on the following code changes: \n\n${diff}"
+    commit_message="$(sgpt "${query}")"
+    printf "%s\n" "$commit_message"
+    read -rp "Do you want to commit your changes with this commit message? [y/N] " response
+    if [[ $response =~ ^[Yy]$ ]]; then
+      git commit -m "$commit_message"
+    else
+      echo "Commit cancelled."
+    fi
   fi
 }
